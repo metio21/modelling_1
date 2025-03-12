281c7c5ddc7535d5800f509f34bd37519c8c6570
Francesco Dessi <fradessi21@gmail.com>
2025-03-12 18:52:58 +0100

    Classes + inheritance

---
+++ colour_point.py
@@ -0,0 +1,32 @@
+from point import Point
+
+class ColorPoint:
+    def __innit__(self,x,y):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def __str__(self):
+        return f"<{self.color}: {self.x}, {self.y}>"
+
+p = ColorPoint(1,2, "red")
+print(p)
+colors = ["red", "green", "blue", "yellow", "orange", "white", "purple"]
+
+color_points = []
+for i in range(10):
+    color_points.append(
+        ColorPoint(random.randint(-10,10),
+                   random.randint(-10, 10),
+                   random.choice(colors)))
+
+print(color_points)
+color_points.sort()
+print(color_points)
+
+
+
+
+
+
+
